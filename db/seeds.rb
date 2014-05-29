04c84735c5223c7279819894633815c8e4b35ad9
Git to da choppa <alexanderdavidpan@gmail.com>
2014-05-29 19:16:44 -0400

    Seed file now creates User entries for Spring Peeper users

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ users = %w[ryan christine reid simon alex
            ruben connor kenny lawrence pei
            siyan tuan nirav payam sahan
            insung mario steven stephen]
+
+users.each do |user|
+  User.create(username: user, password: "yolo")
+end
