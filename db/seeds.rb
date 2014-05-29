bfa8cc031b267e1affd99d1599de28c82f508bb5
Git to da choppa <alexanderdavidpan@gmail.com>
2014-05-29 19:14:41 -0400

    Added array of users to seed file

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+users = %w[ryan christine reid simon alex
+           isabel justin neal beverly zach
+           ruben connor kenny lawrence pei
+           siyan tuan nirav payam sahan
+           insung mario steven stephen]
