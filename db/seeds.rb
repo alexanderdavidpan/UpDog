785f23769130676d51b58301868811f21d11abfc
Alexander Pan <alexanderdavidpan@gmail.com>
2014-05-31 20:00:34 -0400

    Edited seed file so all created users are my friend because I have no friends :(

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-users = %w[ryan christine reid simon alex
+users = %w[alex christine reid simon ryan
            isabel justin neal beverly zach
            ruben connor kenny lawrence pei
            siyan tuan nirav payam sahan
            insung mario steven stephen]
 
 users.each do |user|
-  User.create(username: user, password: "yolo")
+  @user = User.create(username: user, password: "yolo")
+  Friendship.create(friend_one_id: 1, friend_two_id: @user.id, friends: true)
 end
+
+
